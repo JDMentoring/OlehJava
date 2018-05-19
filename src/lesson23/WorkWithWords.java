99285bf4fe2a8b5e15f6d14530e74976f3ce6c53
olehhaliak <olehhaliak@gmail.com>
2018-05-19 17:34:32 +0300

    homeWork23

--- src/lesson23/WorkWithWords.java
+++ src/lesson23/WorkWithWords.java
@@ -3,21 +3,22 @@ package lesson23;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class WorkWithWords {
     public static void main(String[] args) {
-
-        String path = "Games.txt";
-        String lines = readFile(path);
-        System.out.println(lines);
+        String path = "LorenIpsum.txt";
+        String[] words = symbolFilter(readFile(path)).split(" ");
+        TreeSet<String> treeSet = arrToTreeSet(words);
+        getCountOfUniqueWords(treeSet);
     }
 
-    public  static String readFile(String path){
+    public static String readFile(String path) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        try{
+        try {
             Scanner scn = new Scanner(new File(path));
-            while(scn.hasNext()){
+            while (scn.hasNext()) {
                 stringBuilder.append(scn.nextLine());
 
             }
@@ -26,4 +27,30 @@ public class WorkWithWords {
             return null;
         }
     }
+
+    public static String symbolFilter(String s) {
+        String r = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ',' &&
+                    s.charAt(i) != '.' &&
+                    s.charAt(i) != '?' &&
+                    s.charAt(i) != '!' &&
+                    s.charAt(i) != '-') r += s.charAt(i);
+        }
+        return r;
+    }
+
+    public static TreeSet<String> arrToTreeSet(String[] strArr){
+        TreeSet<String> temp = new TreeSet<>();
+        for(int i = 0;i<strArr.length;i++){
+            temp.add(strArr[i]);
+        }
+        return  temp;
+    }
+
+    public static void getCountOfUniqueWords(TreeSet<String> treeSet){
+        System.out.println("This text contains "+treeSet.size()+" unique words.");
+    }
+
+
 }
